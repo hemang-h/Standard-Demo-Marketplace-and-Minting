c7a12d35f04d2c86935d2147d0edf8b12e6b6e57
hemang-h <hemangcertificate@gmail.com>
2022-02-04 22:19:59 +0530

    Modified

--- components/Header.js
+++ components/Header.js
@@ -7,15 +7,15 @@ import { CgProfile } from 'react-icons/cg'
 import { MdOutlineAccountBalanceWallet } from 'react-icons/md'
 
 const style = {
-  wrapper: `bg-[#04111d] w-screen px-[1.2rem] py-[0.8rem] flex `,
+  wrapper: `bg-[#6b0369] w-screen px-[1.2rem] py-[0.8rem] flex `,
   logoContainer: `flex items-center cursor-pointer`,
   logoText: ` ml-[0.8rem] text-white font-semibold text-2xl`,
   searchBar: `flex flex-1 mx-[0.8rem] w-max-[520px] items-center bg-[#363840] rounded-[0.8rem] hover:bg-[#4c505c]`,
-  searchIcon: `text-[#8a939b] mx-3 font-bold text-lg`,
+  searchIcon: `text-[#7a826e] mx-3 font-bold text-lg`,
   searchInput: `h-[2.6rem] w-full border-0 bg-transparent outline-0 ring-0 px-2 pl-0 text-[#e6e8eb] placeholder:text-[#8a939b]`,
   headerItems: ` flex items-center justify-end`,
-  headerItem: `text-white px-4 font-bold text-[#c8cacd] hover:text-white cursor-pointer`,
-  headerIcon: `text-[#8a939b] text-3xl font-black px-4 hover:text-white cursor-pointer`,
+  headerItem: `text-white px-4 font-bold text-[#a8a30d] hover:text-white cursor-pointer`,
+  headerIcon: `text-[#a8a30d] text-3xl font-black px-4 hover:text-white cursor-pointer`,
 }
 
 const Header = () => {
@@ -23,8 +23,8 @@ const Header = () => {
     <div className={style.wrapper}>
       <Link href="/">
         <div className={style.logoContainer}>
-          <Image src={TokenMindsLogo} height={40} width={40} />
-          <div className={style.logoText}>TokenMindsLogo</div>
+          <Image src={TokenMindsLogo} height={50} width={50} />
+          <div className={style.logoText}>TokenMinds</div>
         </div>
       </Link>
       <div className={style.searchBar}>
