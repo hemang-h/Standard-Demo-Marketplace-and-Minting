6aa02cba73e72f728a18bee48e556e4d6de9d035
hemang-h <hemangcertificate@gmail.com>
2022-02-06 02:36:50 +0530

    Added marketplace SDK

--- pages/collections/[collectionId].js
+++ pages/collections/[collectionId].js
@@ -70,7 +70,7 @@ const Collection = () => {
       'https://eth-rinkeby.alchemyapi.io/v2/G--kvnNcpDx08GKWBappKdm671mE9Vjk'
     )
     return sdk.getMarketplaceModule(
-      '0x93A771F7ce845C33381f677489cF21a5964EDD0b'
+      '00x7028739D8715d6b99A73E97378438036DfCacA0f'
     )
   }, [provider])
 
