037caf370a180f902863e622ed579191066d77ec
hemang-h <hemangcertificate@gmail.com>
2022-02-07 02:22:33 +0530

    Completed

--- pages/nfts/[nftId].js
+++ pages/nfts/[nftId].js
@@ -64,4 +64,30 @@ const Nft = () => {
     })()
   }, [marketPlaceModule])
 
-  
+  return (
+    <div>
+      <Header />
+      <div className={style.wrapper}>
+        <div className={style.container}>
+          <div className={style.topContent}>
+            <div className={style.nftImgContainer}>
+              <NFTImage selectedNft={selectedNft} />
+            </div>
+            <div className={style.detailsContainer}>
+              <GeneralDetails selectedNft={selectedNft} />
+              <Purchase
+                isListed={router.query.isListed}
+                selectedNft={selectedNft}
+                listings={listings}
+                marketPlaceModule={marketPlaceModule}
+              />
+            </div>
+          </div>
+          <ItemActivity />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Nft
